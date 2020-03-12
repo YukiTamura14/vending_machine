f5457eb7d41efde3491f79473dc284633dd5de0f
micchan <p1r2yy9knr@gmail.com>
2020-03-12 17:05:46 +0900

    クラスをVendingMachineとDrinkだけにした

--- vending_machine(master).rb
+++ vending_machine(master).rb
@@ -11,21 +11,26 @@
 # 投入金額の合計を表示
 # vm.total
 # stock内のジュース情報を表示
-# vm.sale_info(stock)
+# vm.sale_info
 # 売り切れ表示
-# vm.sold_out(stock)
+# vm.sold_out
 # 投入したお金を払い戻し
 # vm.refund
 # ジュースを買う
-# vm.purchase(stock)
+# vm.purchase('コーラ')
 # 売り上げ金額を表示する
 # vm.sale_amount
 
 class VendingMachine
   MONEY = [10, 50, 100, 500, 1000].freeze
+  attr_reader :total, :sale_amount
   def initialize
     @total = 0
     @sale_amount = 0
+    @coke = Drink.new("コーラ", 120, 5)
+    @red_bull = Drink.new("レッドブル", 200, 5)
+    @water = Drink.new("水", 100, 5)
+    @drink_table = [@coke, @red_bull, @water]
   end
   def insert(money)
     if MONEY.include?(money)
@@ -34,14 +39,8 @@ class VendingMachine
       money
     end
   end
-  def total
-    @total
-  end
   def reset
     @total = 0
-  end
-  def sale_amount
-    @sale_amount
   end
   def add_sale_amount=(money)
     @sale_amount += money
@@ -51,79 +50,64 @@ class VendingMachine
     @total = 0
     refunded_money
   end
-  def sale_info(stock)
-    # puts "ボタン光ってます"
-    stock.get_drink_table.each do |drink|
-      if drink.stock > 0 && drink.price <= @total
-        p "#{drink.name}, #{drink.price}円"
-      else
-        return
+  def sale_info
+    @drink_table.map do |drink|
+      if drink.stock_count > 0 && drink.price <= @total
+        "#{drink.name}, #{drink.price}円"
+    end
+  end
+  def sold_out
+    @drink_table.map do |drink|
+      if drink.stock_count == 0
+       "#{drink.name}, #{drink.price}円"
       end
     end
   end
-  def sold_out(stock)
-    stock.get_drink_table.each do |drink|
-      if drink.stock == 0
-        p "#{drink.name}, #{drink.price}円"
+  def purchase(drinkname)
+    # drink = Choice.drink_choice(stock.get_drink_table)
+    # if drink.stock > 0 && drink.price <= @total
+    #   change = @total - drink.price
+    #   self.reset
+    #   stock.reduce(drink)
+    #   self.add_sale_amount=(drink.price)
+    #   puts "#{drink.name}を買いました"
+    #   puts "#{change}円のお釣りです"
+    # elsif drink.stock == 0
+    #   puts "売り切れごめんね"
+    # elsif drink.price > @total
+    #   puts "お金が足りないよ"
+    # end
+    @drink_table.each do |drink|
+      if drink.name.include?(drinkname)
+        if drink.stock_count.zero?
+          return "売り切れ"
+        elsif drink.price > @total
+          return "お金足りないよ"
+        else
+          change = @total - drink.price
+          self.reset
+          drink.stock_count -= 1
+          self.drink.add_sale_amount=(drink.price)
+          return "#{drink.name}, お釣り: #{change}円"
+        end
       else
-        return
+        "ナッシング"
       end
     end
   end
-  def purchase(stock)
-    drink = Choice.drink_choice(stock.get_drink_table)
-    if drink.stock > 0 && drink.price <= @total
-      change = @total - drink.price
-      self.reset
-      stock.reduce(drink)
-      self.add_sale_amount=(drink.price)
-      puts "#{drink.name}を買いました"
-      puts "#{change}円のお釣りです"
-    elsif drink.stock == 0
-      puts "売り切れごめんね"
-    elsif drink.price > @total
-      puts "お金が足りないよ"
-    end
+
+  def stock(name, stock_count)
+    
   end
+
+  private :add_sale_amount
+
 end
 class Drink
-  attr_accessor :name, :price, :stock
-  def initialize(name, price)
+  attr_accessor :name, :price, :stock_count
+  def initialize(name, price, stock_count)
     @name = name
     @price = price
-    @stock = 5
-  end
-end
-class Stock
-  def initialize
-    @coke = Drink.new("コーラ", 120)
-    @red_bull = Drink.new("レッドブル", 200)
-    @water = Drink.new("水", 100)
-    @drink_table = [@coke, @red_bull, @water]
-  end
-  def get_drink_table
-    @drink_table
-  end
-  def reduce(drink)
-    @coke.stock -= 1 if @coke.name == drink.name
-    @red_bull.stock -= 1 if @red_bull.name == drink.name
-    @water.stock -= 1 if @water.name == drink.name
-  end
-end
-class Choice
-  def self.drink_choice(drink_table)
-    drinks = drink_table
-    puts "コーラ：1、レッドブル：2、水：3"
-    answer = gets.to_i
-    case answer
-      when 1
-        drink = drinks[0]
-      when 2
-        drink = drinks[1]
-      when 3
-        drink = drinks[2]
-      else
-        self.drink_choice
-    end
+    @stock_count = stock_count
   end
 end
