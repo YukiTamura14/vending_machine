4902d5c70e4b4a467b398cad4d59744b23c1b57b
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-03-13 18:53:16 +0900

    2020年3月13日付最新コード差し替え

--- vending_machine(20200313).rb
+++ vending_machine(20200313).rb
@@ -1,103 +1,66 @@
-# 使い方
-# irbを起動
-# require '/Users/tatsuyamatsuhashi/workspace/vending_machine/vending_machine(master).rb'
-# （↑のパスは、自動販売機ファイルが入っているパスを指定する）
-# 初期設定（自動販売機インスタンスと在庫インスタンスを作成して、vmという変数に代入する）
+# require '/Users/tatsuyamatsuhashi/workspace/vending_machine/maa_vm.rb' または
+# load '/Users/tatsuyamatsuhashi/workspace/vending_machine/maa_vm.rb'
 # vm = VendingMachine.new
-# 作成した自動販売機に500円を入れる
-# vm.insert(500)
-# 投入金額の合計を表示
-# vm.total
-# 購入可能なジュースを表示
-# vm.sale_info
-# 売り切れ表示
-# vm.sold_out
-# 投入したお金を払い戻し
-# vm.refund
-# ジュースを買う
-# vm.purchase('コーラ')
-# 売り上げ金額を表示する
-# vm.sale_amount
-# ジュースのストックを追加
-# vm.stock(drink_name: 'コーラ', stock_count: 5)
-
+# vm.store 格納する
+# vm.drinks 格納されているドリンクの情報を表示する
+# vm.insert(500) 投入する
+# vm.payback 払い戻す
+# vm.total 投入金額の総計を表示
+# vm.purchasable? 購入できる商品を表示
+# vm.purchase('cola') 購入する
+# vm.sale_amount 現在の売上金額の表示
 class VendingMachine
   MONEY = [10, 50, 100, 500, 1000].freeze
-  attr_reader :total, :sale_amount
+  attr_reader :total, :sale_amount, :drinks
   def initialize
     @total = 0
     @sale_amount = 0
-    @coke = Drink.new("コーラ", 120, 5)
-    @red_bull = Drink.new("レッドブル", 200, 5)
-    @water = Drink.new("水", 100, 5)
-    @drink_table = [@coke, @red_bull, @water]
+    @drinks = []
   end
-  def insert(money)
+  def insert(money) #投入する
     if MONEY.include?(money)
       @total += money
-    else
-      money
     end
   end
-  def refund
-    refunded_money = @total
-    @total = 0
-    refunded_money
+  def payback #払い戻す
+    @total.tap{@total = 0}
   end
-  def sale_info
-    sale_drinks = @drink_table.map do |drink|
-      if drink.stock_count > 0 && drink.price <= @total
-        drink_info(drink)
+  def store #格納する
+    cola = Drink.new('cola', 120, 5)
+    red_bull = Drink.new('Red Bull', 200, 5)
+    water = Drink.new('water', 100, 5)
+    @drinks = [cola, red_bull, water]
+  end
+  def purchasable? #購入できるかどうかの確認
+    purchasable_drinks = []
+    @drinks.each do |drink|
+      if @total >= drink.price && drink.stock > 0
+        purchasable_drinks.push(drink.name)
       end
     end
-    sale_drinks.compact
+    purchasable_drinks
   end
-  def sold_out
-    sold_out_drinks = @drink_table.map do |drink|
-      if drink.stock_count == 0
-        drink_info(drink)
-      end
-    end
-    sold_out_drinks.compact
-  end
-  def purchase(drink_name)
-    @drink_table.each do |drink|
-      if drink.name.include?(drink_name)
-        if drink.stock_count.zero?
-          return "売り切れだよ"
-        elsif drink.price > @total
-          return "お金足りないよ"
+  def purchase(drink_name) #購入する
+    @drinks.each do |drink|
+      if drink.name == drink_name
+        if @total < drink.price || drink.stock == 0
+          return false
         else
-          change = @total - drink.price
-          @total = 0
-          drink.stock_count -= 1
-          add_sale_amount(drink.price)
-          return "#{drink.name}, お釣り: #{change}円"
+          @total -= drink.price
+          drink.stock -= 1
+          @sale_amount += drink.price
+          return self.payback
         end
       end
     end
-    "そんなジュース売ってないよ"
   end
-  def stock(drink_name:, stock_count:)
-    @drink_table.each_with_index do |drink, index|
-      if drink.name.include?(drink_name) && Integer === stock_count
-        @drink_table[index].stock_count += stock_count
-      end
-    end
-  end
-  private
-  def add_sale_amount(money)
-    @sale_amount += money
-  end
-  def drink_info(drink)
-    "#{drink.name}, #{drink.price}円"
-  end
+  false
 end
 class Drink
-  attr_accessor :name, :price, :stock_count
-  def initialize(name, price, stock_count)
+  attr_accessor :name, :price, :stock
+  def initialize(name, price, stock)
     @name = name
     @price = price
-    @stock_count = stock_count
+    @stock = stock
   end
 end
